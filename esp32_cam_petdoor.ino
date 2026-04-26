418b68a7cc65bd177c90bfdcef86cfa8c7d31184
TacoCode64 <leonadrian664@gmail.com>
2026-04-26 13:06:34 -0500

    Update WiFi credentials for ESP32 camera

--- esp32_cam_petdoor.ino
+++ esp32_cam_petdoor.ino
@@ -39,8 +39,8 @@
 // ---------------------------------------------------------------------------
 // Configuration  ← EDIT THESE
 // ---------------------------------------------------------------------------
-const char* WIFI_SSID     = "g3283789";
-const char* WIFI_PASSWORD = "866694295";
+const char* WIFI_SSID     = "Dollywood";
+const char* WIFI_PASSWORD = "Dolly7338";
 
 // NTP time server for timestamping SD card files
 const char* NTP_SERVER    = "pool.ntp.org";
